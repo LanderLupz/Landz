92c09bdd4b921b533363c79295e1dc9e7ae4846a
Lander Lupango <jlanderlupango@gmail.com>
2023-02-15 19:48:07 +0800

    just trying hayst4

--- jscript/predict202.js
+++ jscript/predict202.js
@@ -45,7 +45,7 @@ $("#image-selector").change(function () {
 let model;
 (async function () {
 	
-	model = await tf.loadModel('https://github.com/LanderLupz/Landz/blob/main/model_final/model.json');
+	model = await tf.loadModel("https://github.com/LanderLupz/Landz/blob/main/model_final/model.json");
 	$("#selected-image").attr("src", "https://github.com/LanderLupz/Landz/tree/main/assets/NV.jpg")
 	
 	
