7ac0c095d86b4ebb7c9319af4160b47e8c8e7236
Lander Lupango <jlanderlupango@gmail.com>
2023-02-15 18:26:40 +0800

    trial and error

--- jscript/predict202.js
+++ jscript/predict202.js
@@ -45,7 +45,7 @@ $("#image-selector").change(function () {
 let model;
 (async function () {
 	
-	model = await tf.loadModel('http://localhost/SkinAnalyzer/model_final/model1.json');
+	model = await tf.loadModel('https://github.com/LanderLupz/Landz/blob/main/model_final/model.json');
 	$("#selected-image").attr("src", "http://localhost/SkinAnalyzer/assets/skind.jpg")
 	
 	
