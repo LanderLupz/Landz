a2276a34fe84636b169a4c78e25c218637163fb3
Lander Lupango <jlanderlupango@gmail.com>
2023-02-15 19:03:17 +0800

    just trying again

--- jscript/app_startup_code.js
+++ jscript/app_startup_code.js
@@ -33,7 +33,7 @@ function predictOnLoad() {
 let model;
 (async function () {
 	
-	model = await tf.loadModel('http://concept.test.woza.work/final_model_kaggle_version1/model.json');
+	model = await tf.loadModel('https://github.com/LanderLupz/Landz/blob/main/model_final/model.json');
 	$("#selected-image").attr("src", "http://localhost/SkinAnalyzer/assets/skind.jpg");
 	
 	// Hide the model loading spinner
--- jscript/predict202.js
+++ jscript/predict202.js
@@ -45,7 +45,7 @@ $("#image-selector").change(function () {
 let model;
 (async function () {
 	
-	model = await tf.loadModel('http://concept.test.woza.work/final_model_kaggle_version1/model.json');
+	model = await tf.loadModel('https://github.com/LanderLupz/Landz/blob/main/model_final/model.json');
 	$("#selected-image").attr("src", "http://localhost/SkinAnalyzer/assets/skind.jpg")
 	
 	
